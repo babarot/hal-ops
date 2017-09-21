626994a6acc16f2e68a7e7af77e3e0f847108cf9
b4b4r07 <b4b4r07@gmail.com>
2017-09-21 17:46:01 +0900

    commit

---
+++ cmd/check.go
@@ -0,0 +1,93 @@
+package cmd
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"strings"
+
+	"github.com/b4b4r07/hal-ops/command"
+	"github.com/spf13/cobra"
+)
+
+var checkCmd = &cobra.Command{
+	Use:   "check",
+	Short: "check",
+	Long:  "check",
+	RunE:  check,
+}
+
+func convertString(b []byte) string {
+	return strings.TrimSuffix(string(b), "\n")
+}
+
+func copyFile(src, dst string) error {
+	in, err := os.Open(src)
+	if err != nil {
+		return err
+	}
+	defer in.Close()
+
+	out, err := os.Create(dst)
+	if err != nil {
+		return err
+	}
+	defer out.Close()
+
+	_, err = io.Copy(out, in)
+	if err != nil {
+		return err
+	}
+	return out.Close()
+}
+
+func check(cmd *cobra.Command, args []string) error {
+	c := command.New("bash hoge.sh")
+	if err := c.Run(); err != nil {
+		panic(err)
+	}
+	res := c.Result()
+	fmt.Println(res.StdoutString())
+	fmt.Printf("%#v %#v %#v\n", res.StdoutString(), res.StderrString(), res)
+	return nil
+
+	/*
+		var branch string
+		if len(args) > 0 {
+			branch = args[0]
+			if err := cli.Run("git", "checkout", branch); err != nil {
+				return err
+			}
+		}
+		var stdout, stderr bytes.Buffer
+		c := exec.Command("git", "rev-parse", "--abbrev-ref", "HEAD")
+		c.Stdout = &stdout
+		c.Stderr = &stderr
+		if err := c.Run(); err != nil {
+			return err
+		}
+		if stderr.Len() > 0 {
+			return fmt.Errorf("hoge")
+		}
+
+		branch = convertString(stdout.Bytes())
+		if branch == "master" {
+			return fmt.Errorf("Error: current branch is master")
+		}
+
+		// if err := cli.Run("cp", ".hal/config", filepath.Join(os.Getenv("HOME"), ".hal", "config")); err != nil {
+		// 	return err
+		// }
+
+		dst := filepath.Join(os.Getenv("HOME"), ".hal", "config")
+		if err := copyFile(".hal/config", dst); err != nil {
+			return err
+		}
+
+		return cli.Run("hal", "config")
+	*/
+}
+
+func init() {
+	RootCmd.AddCommand(checkCmd)
+}
---
+++ hoge.sh
@@ -0,0 +1,4 @@
+echo ok
+echo ng >&2
+sleep 1
+echo end
